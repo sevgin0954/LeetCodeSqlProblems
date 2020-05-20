8a37904d0f95a820bde6af890f9eefd8bfc0ecb8
sevgin0954 <skateboardista_0954@abv.bg>
2020-05-20 22:53:33 +0300

    added solutions

---
+++ 1179.ReformatDepartmentTable.sql
@@ -0,0 +1,21 @@
+SELECT * FROM (
+	SELECT id, revenue, month + '_Revenue' as month FROM Department
+) AS Tbl
+PIVOT(
+	MAX(revenue)
+	FOR month IN 
+	(
+		Jan_Revenue,
+		Feb_Revenue,
+		Mar_Revenue,
+		Apr_Revenue,
+		May_Revenue,
+		Jun_Revenue,
+		Jul_Revenue,
+		Aug_Revenue,
+		Sep_Revenue,
+		Oct_Revenue,
+		Nov_Revenue,
+		Dec_Revenue
+	)
+) AS PivotTbl
